41cc97981028d72b5517776e9b32d2c1ec2eabb1
Ricard Torres <ricard.torres@netcentric.biz>
2019-08-05 14:51:09 +0200

    Change scripts

---
+++ docs/precache-manifest.ce97230eedea4aed469b3f37aca555b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dae14816b32497fa7604",
+    "url": "/js/chunk-vendors.4badcd3a.js"
+  },
+  {
+    "revision": "27ec156563574a0fb572",
+    "url": "/js/app.aea48024.js"
+  },
+  {
+    "revision": "29d3856aadcec68af03f",
+    "url": "/js/about.ec8a848d.js"
+  },
+  {
+    "revision": "16c0f9806e07ff23a0e401c1bfe96141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dae14816b32497fa7604",
+    "url": "/css/chunk-vendors.3a837761.css"
+  },
+  {
+    "revision": "27ec156563574a0fb572",
+    "url": "/css/app.6febd8b8.css"
+  }
+];
