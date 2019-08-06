2ac1e07427408f2453fd8342e725a36c089a89c6
Ricard Torres <ricard.torres@netcentric.biz>
2019-08-06 13:10:01 +0200

    Add footer with version
    Fixes #16

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { rtdbPlugin } from 'vuefire';
 // Register global Bootstrap VUE components
 import {
   BAlert,
+  BContainer,
+  BRow,
+  BCol,
 } from 'bootstrap-vue';
 
 import App from './App.vue';
@@ -19,6 +22,9 @@ Vue.use(rtdbPlugin);
 
 // Init the Bootstrap Components previously imported
 Vue.component('b-alert', BAlert);
+Vue.component('b-container', BContainer);
+Vue.component('b-row', BRow);
+Vue.component('b-col', BCol);
 
 new Vue({
   router,
