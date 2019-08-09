97d63254a0f47532cc66e9143c6a117e6817acb4
Ricard Torres <ricard.torres@netcentric.biz>
2019-08-09 14:41:05 +0200

    Create precache-manifest.f1e15761521532ef3041a1463b11902b.js

---
+++ docs/precache-manifest.f1e15761521532ef3041a1463b11902b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/workout-app/robots.txt"
+  },
+  {
+    "revision": "cd7d798324fe2366dba1",
+    "url": "/workout-app/js/chunk-vendors.fdc827d5.js"
+  },
+  {
+    "revision": "17361aabe906313d1706",
+    "url": "/workout-app/js/app.13f4fee1.js"
+  },
+  {
+    "revision": "ee3535ee16beaca457e1",
+    "url": "/workout-app/js/about.eccccffc.js"
+  },
+  {
+    "revision": "d3bca8c8135d5f4133c364003e4a2bfd",
+    "url": "/workout-app/index.html"
+  },
+  {
+    "revision": "f3257d6bac0d2f59ca84f14514ab85f4",
+    "url": "/workout-app/img/logo.f3257d6b.svg"
+  },
+  {
+    "revision": "17361aabe906313d1706",
+    "url": "/workout-app/css/app.beba60d6.css"
+  }
+];
