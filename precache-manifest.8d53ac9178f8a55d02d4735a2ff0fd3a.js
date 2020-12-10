67fe1c6c1ea467a8e2605691d943a1570dbc85de
quicoto <torres.rick@gmail.com>
2020-12-10 16:10:33 +0000

    Deploying to gh-pages from  @ 4e139ffc87c74fcae1d8a44ddc2a720cb586e331 🚀

---
+++ precache-manifest.8d53ac9178f8a55d02d4735a2ff0fd3a.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa2dff34e93eba7291b",
+    "url": "/workout-app/css/admin.6a396e59.css"
+  },
+  {
+    "revision": "6093d0a22c6d3c694ca0",
+    "url": "/workout-app/css/app.d1ccdf0e.css"
+  },
+  {
+    "revision": "2cef68cc1e51e6981f46",
+    "url": "/workout-app/css/start.87e138e5.css"
+  },
+  {
+    "revision": "f45b0cd5f19090e56d02cf20052cd9c1",
+    "url": "/workout-app/data.json"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/workout-app/fonts/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/workout-app/fonts/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/workout-app/fonts/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/workout-app/fonts/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/workout-app/fonts/montserrat-v14-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/workout-app/fonts/montserrat-v14-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/workout-app/fonts/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/workout-app/fonts/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "26d42c9428780e545a540bbb50c84bce",
+    "url": "/workout-app/fonts/montserrat-v14-latin-900.26d42c94.woff"
+  },
+  {
+    "revision": "58cd789700850375b834e8b6776002eb",
+    "url": "/workout-app/fonts/montserrat-v14-latin-900.58cd7897.woff2"
+  },
+  {
+    "revision": "3df2c011bc1447742a11e469346da105",
+    "url": "/workout-app/img/exercises/1.jpg"
+  },
+  {
+    "revision": "c04450bf730746c4211449b37590ba10",
+    "url": "/workout-app/img/exercises/13.jpg"
+  },
+  {
+    "revision": "07967f1598c063bec50d0aeb68cbfb51",
+    "url": "/workout-app/img/exercises/14.jpg"
+  },
+  {
+    "revision": "07967f1598c063bec50d0aeb68cbfb51",
+    "url": "/workout-app/img/exercises/15.jpg"
+  },
+  {
+    "revision": "09f6b6f6a8e8722d568af78325103787",
+    "url": "/workout-app/img/exercises/2.jpg"
+  },
+  {
+    "revision": "e7843242fba095e4cc842f0669411f52",
+    "url": "/workout-app/img/exercises/3.jpg"
+  },
+  {
+    "revision": "b07f833de24fe29d74d95f90ae33a88b",
+    "url": "/workout-app/img/exercises/4.jpg"
+  },
+  {
+    "revision": "62a25fa5b1f67ee1814ad0102959a1f3",
+    "url": "/workout-app/img/exercises/5.jpg"
+  },
+  {
+    "revision": "45348c31723978b2116c57c96ed0e323",
+    "url": "/workout-app/img/exercises/6.jpg"
+  },
+  {
+    "revision": "6e478fbf32a58d4d3e9d35d49cd1459f",
+    "url": "/workout-app/img/exercises/7.jpg"
+  },
+  {
+    "revision": "9b579019c67d97ff7c4505d9da419759",
+    "url": "/workout-app/img/exercises/9.jpg"
+  },
+  {
+    "revision": "e62d67c0294d87075a338d415e9d5ff9",
+    "url": "/workout-app/img/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "bef52cf8ae112043726f2e680d6d5cae",
+    "url": "/workout-app/img/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "17babcaa897f09175eb85868c4795524",
+    "url": "/workout-app/img/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "807923b65f3aa66ec7ff361c628400b4",
+    "url": "/workout-app/img/favicons/browserconfig.xml"
+  },
+  {
+    "revision": "063be3e988db32da31c1deb4ecf2f52b",
+    "url": "/workout-app/img/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "2e109903ecd966e282059bed98efbbb3",
+    "url": "/workout-app/img/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "8d0fd06b9dd9f72869c1ff8b66a7dbdb",
+    "url": "/workout-app/img/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "d04a7f94fd2bd1baf9258229bfb51b49",
+    "url": "/workout-app/img/favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "f3257d6bac0d2f59ca84f14514ab85f4",
+    "url": "/workout-app/img/logo.f3257d6b.svg"
+  },
+  {
+    "revision": "e9c23f45af56718b5370440eb39648c1",
+    "url": "/workout-app/index.html"
+  },
+  {
+    "revision": "1fa2dff34e93eba7291b",
+    "url": "/workout-app/js/admin.033c0d4c.js"
+  },
+  {
+    "revision": "6093d0a22c6d3c694ca0",
+    "url": "/workout-app/js/app.09641b02.js"
+  },
+  {
+    "revision": "8827b2affa8c37aa7b74",
+    "url": "/workout-app/js/chunk-vendors.a922f8d5.js"
+  },
+  {
+    "revision": "ab12b909b3bbae338196",
+    "url": "/workout-app/js/exercises.27fae620.js"
+  },
+  {
+    "revision": "41b8392b960527399ee4",
+    "url": "/workout-app/js/exercises~search.083e2517.js"
+  },
+  {
+    "revision": "b489fee1b408126f5d04",
+    "url": "/workout-app/js/search.8172b64c.js"
+  },
+  {
+    "revision": "2cef68cc1e51e6981f46",
+    "url": "/workout-app/js/start.cdbbc6e8.js"
+  },
+  {
+    "revision": "d307293ffa87ae4d93ac",
+    "url": "/workout-app/js/workouts.25b3f206.js"
+  },
+  {
+    "revision": "1a71e805db98fbc640bcdcc5aba7a7ec",
+    "url": "/workout-app/manifest.json"
+  },
+  {
+    "revision": "56c9031ae238a11e7e7ca37476d93889",
+    "url": "/workout-app/videos/muted-blank.mp4"
+  },
+  {
+    "revision": "01efc4789be70266fda42324dad1fd95",
+    "url": "/workout-app/videos/muted-blank.ogv"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16eaee4676a04f7eacace704b2c5bf5a.js"
+  "/workout-app/precache-manifest.8d53ac9178f8a55d02d4735a2ff0fd3a.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "thorkout"});
+workbox.core.setCacheNameDetails({prefix: "workoutapp"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
